78c8974ee688893cae2e710f15383da1ff010d75
unknown <srijanaryal5@gmail.com>
2024-05-21 21:50:01 +0545

    code review

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const db = "mongodb+srv://ranjana:ranjana123@bakeryshop.9vlz7wx.mongodb.net/";
+// const db = "mongodb+srv://ranjana:ranjana123@bakeryshop.9vlz7wx.mongodb.net/";
+const db ="mongodb+srv://rajanathapa98:rajana123@cluster0.058p3fj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 module.exports = connect = async () => {
   try {
     //database = cakeshop
